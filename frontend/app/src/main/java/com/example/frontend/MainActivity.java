076f3b53f5e15411e1aa1b94e2458d7cae458e64
HanSolo.CAU <hansnam123@gmail.com>
2021-09-24 23:23:28 +0900

    지도 point 표출

--- frontend/app/src/main/java/com/example/frontend/MainActivity.java
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.java
@@ -22,6 +22,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.frontend.databinding.ActivityMainBinding;
 
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
@@ -63,6 +66,28 @@ public class MainActivity extends AppCompatActivity {
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(7, true);
+
+        // 줌 인
+        mapView.zoomIn(true);
+
+        // 줌 아웃
+        mapView.zoomOut(true);
+
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633);
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
 
     }
 
