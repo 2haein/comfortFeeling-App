f4f12bf6e2d318589c4be6f364a0114a6fa88bfb
2haein <eehaein2@gmail.com>
2021-10-01 16:53:03 +0900

    [설정] properties변경 및 entityScan 설정

--- backend/src/main/java/com/codeboogie/comfortbackend/ComfortFeelingBackendApplication.java
+++ backend/src/main/java/com/codeboogie/comfortbackend/ComfortFeelingBackendApplication.java
@@ -2,7 +2,11 @@ package com.codeboogie.comfortbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan("com.codeboogie.comfortbackend")
+@EnableJpaRepositories("com.codeboogie.comfortbackend")
 @SpringBootApplication
 public class ComfortFeelingBackendApplication {
     public static void main (String[] args) {
