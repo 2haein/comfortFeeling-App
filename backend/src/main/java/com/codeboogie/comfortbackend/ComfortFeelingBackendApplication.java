abf374356fccecc3ef56239dd38ccb63f2f6bbea
2haein <eehaein2@gmail.com>
2021-10-01 16:37:57 +0900

    [설정] application.yml DataSource autoconfigure 제외

--- backend/src/main/java/com/codeboogie/comfortbackend/ComfortFeelingBackendApplication.java
+++ backend/src/main/java/com/codeboogie/comfortbackend/ComfortFeelingBackendApplication.java
@@ -2,10 +2,8 @@ package com.codeboogie.comfortbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ComfortFeelingBackendApplication {
     public static void main (String[] args) {
         SpringApplication.run(ComfortFeelingBackendApplication.class, args);
