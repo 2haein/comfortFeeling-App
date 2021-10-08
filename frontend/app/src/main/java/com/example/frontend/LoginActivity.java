2100d9c97695c9470cf7c80ff38d54b19b94940d
2haein <eehaein2@gmail.com>
2021-10-08 13:59:55 +0900

    [기능]: 로그인 세션 존재시 자동 로그인 구현

--- frontend/app/src/main/java/com/example/frontend/LoginActivity.java
+++ frontend/app/src/main/java/com/example/frontend/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -14,12 +15,19 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.frontend.callback.SessionCallback;
+import com.kakao.auth.ApiErrorCode;
 import com.kakao.auth.AuthType;
+import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
 
 public class LoginActivity extends AppCompatActivity {
     private ImageButton loginV1;
@@ -37,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
 
         session = Session.getCurrentSession();
         session.addCallback(sessionCallback);
+        session.checkAndImplicitOpen();
 
         loginV1.setOnClickListener(v -> {
             if (Session.getCurrentSession().checkAndImplicitOpen()) {
@@ -44,9 +53,6 @@ public class LoginActivity extends AppCompatActivity {
                 // 카카오 로그인 시도 (창이 안뜬다.)
                 sessionCallback.requestMe();
 
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivityForResult(intent, 100);
-                onActivityResult(100,101, intent);
             } else {
                 Log.d(TAG, "onClick: 로그인 세션끝남");
                 // 카카오 로그인 시도 (창이 뜬다.)
@@ -87,10 +93,99 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.i("KAKAO_API", "onActivityResult " + requestCode+ ":"+ resultCode+"::"+ data);
         // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            Log.i("KAKAO_API", "onActivityResult2 " + requestCode+  ":"+ resultCode+"::"+ data);
             super.onActivityResult(requestCode, resultCode, data);
             return;
         }
     }
+
+    private class SessionCallback implements ISessionCallback {
+
+        // 로그인에 성공한 상태
+        @Override
+        public void onSessionOpened() {
+            requestMe();
+        }
+
+        // 로그인에 실패한 상태
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: "+exception.toString(), Toast.LENGTH_SHORT).show();
+            Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+        }
+
+        // 사용자 정보 요청
+        public void requestMe() {
+            UserManagement.getInstance()
+                    .me(new MeV2ResponseCallback() {
+                        @Override
+                        public void onSessionClosed(ErrorResult errorResult) {
+                            Toast.makeText(getApplicationContext(),"세션이 닫혔습니다. 다시 시도해 주세요: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                            Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                        }
+
+                        @Override
+                        public void onFailure(ErrorResult errorResult) {
+                            int result = errorResult.getErrorCode();
+
+                            if(result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                                Toast.makeText(getApplicationContext(), "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                Toast.makeText(getApplicationContext(),"로그인 도중 오류가 발생했습니다: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                            }
+
+                            Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
+                        }
+
+                        @Override
+                        public void onSuccess(MeV2Response result) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+
+                            Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+                            String id = String.valueOf(result.getId());
+
+                            UserAccount kakaoAccount = result.getKakaoAccount();
+                            if (kakaoAccount != null) {
+
+                                // 이메일
+                                String email = kakaoAccount.getEmail();
+
+                                // 프로필 (닉네임, 프로필 사진 경로)
+                                Profile profile = kakaoAccount.getProfile();
+                                intent.putExtra("name", profile.getNickname());
+                                intent.putExtra("profile", profile.getProfileImageUrl());
+                                intent.putExtra("thumbnail", profile.getThumbnailImageUrl());
+
+                                // LOGGING
+                                if (profile ==null){
+                                    Log.d("KAKAO_API", "onSuccess:profile null ");
+                                }else{
+                                    Log.d("KAKAO_API", "onSuccess:getProfileImageUrl "+profile.getProfileImageUrl());
+                                    Log.d("KAKAO_API", "onSuccess:getThumbnailImageUrl "+profile.getThumbnailImageUrl());
+                                    Log.d("KAKAO_API", "onSuccess:getNickname "+profile.getNickname());
+                                }
+                                if (email != null) {
+                                    Log.d("KAKAO_API", "onSuccess:email "+email);
+                                }
+
+
+                            }else{
+                                Log.i("KAKAO_API", "onSuccess: kakaoAccount null");
+                            }
+
+                            startActivity(intent);
+                            finish();
+
+
+                        }
+                    });
+
+        }
+    }
+
 }
