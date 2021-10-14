b03cb26fa67799e532d1d859775e66c084513bdd
2haein <eehaein2@gmail.com>
2021-10-15 03:02:59 +0900

    기능: 버튼 이모지 추가

--- frontend/app/src/main/java/com/example/frontend/PostActivity.java
+++ frontend/app/src/main/java/com/example/frontend/PostActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,7 +29,9 @@ public class PostActivity extends AppCompatActivity {
     public Button back_btn;
     public Button submit_btn;
     public RatingBar feel_rate;
+    public ImageView feel_btn1, feel_btn2, feel_btn3, feel_btn4, feel_btn5;
     private SessionCallback sessionCallback = new SessionCallback();
+    private int rating;
 
 
     @Override
@@ -36,16 +40,77 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post);
 
         edit_text = (EditText)findViewById(R.id.editTextTextMultiLine);
-        feel_rate = (RatingBar)findViewById(R.id.ratingBar);
+//        feel_rate = (RatingBar)findViewById(R.id.ratingBar);
+        feel_btn1 = (ImageView)findViewById(R.id.imageView1);
+        feel_btn2 = (ImageView)findViewById(R.id.imageView2);
+        feel_btn3 = (ImageView)findViewById(R.id.imageView3);
+        feel_btn4 = (ImageView)findViewById(R.id.imageView4);
+        feel_btn5 = (ImageView)findViewById(R.id.imageView5);
         back_btn = (Button)findViewById(R.id.back_btn);
         submit_btn = (Button)findViewById(R.id.button);
 
         Intent intent = getIntent();
 
-        LayerDrawable stars = (LayerDrawable) feel_rate.getProgressDrawable();
-        stars.getDrawable(2).setColorFilter(Color.parseColor("#e7c31b"), PorterDuff.Mode.SRC_ATOP); // for filled stars
-        stars.getDrawable(1).setColorFilter(Color.parseColor("#e7c31b"), PorterDuff.Mode.SRC_ATOP); // for half filled stars
-        stars.getDrawable(0).setColorFilter(Color.parseColor("#c2c2c2"), PorterDuff.Mode.SRC_ATOP); // for empty stars
+        feel_btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                feel_btn1.setBackgroundColor(Color.YELLOW);
+                feel_btn2.setBackgroundColor(Color.WHITE);
+                feel_btn3.setBackgroundColor(Color.WHITE);
+                feel_btn4.setBackgroundColor(Color.WHITE);
+                feel_btn5.setBackgroundColor(Color.WHITE);
+                rating = 1;
+            }
+        });
+        feel_btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                feel_btn1.setBackgroundColor(Color.WHITE);
+                feel_btn2.setBackgroundColor(Color.YELLOW);
+                feel_btn3.setBackgroundColor(Color.WHITE);
+                feel_btn4.setBackgroundColor(Color.WHITE);
+                feel_btn5.setBackgroundColor(Color.WHITE);
+                rating = 2;
+            }
+        });
+        feel_btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                feel_btn1.setBackgroundColor(Color.WHITE);
+                feel_btn2.setBackgroundColor(Color.WHITE);
+                feel_btn3.setBackgroundColor(Color.YELLOW);
+                feel_btn4.setBackgroundColor(Color.WHITE);
+                feel_btn5.setBackgroundColor(Color.WHITE);
+                rating = 3;
+            }
+        });
+        feel_btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                feel_btn1.setBackgroundColor(Color.WHITE);
+                feel_btn2.setBackgroundColor(Color.WHITE);
+                feel_btn3.setBackgroundColor(Color.WHITE);
+                feel_btn4.setBackgroundColor(Color.YELLOW);
+                feel_btn5.setBackgroundColor(Color.WHITE);
+                rating = 4;
+            }
+        });
+        feel_btn5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                feel_btn1.setBackgroundColor(Color.WHITE);
+                feel_btn2.setBackgroundColor(Color.WHITE);
+                feel_btn3.setBackgroundColor(Color.WHITE);
+                feel_btn4.setBackgroundColor(Color.WHITE);
+                feel_btn5.setBackgroundColor(Color.YELLOW);
+                rating = 5;
+            }
+        });
+
+//        LayerDrawable stars = (LayerDrawable) feel_rate.getProgressDrawable();
+//        stars.getDrawable(2).setColorFilter(Color.parseColor("#e7c31b"), PorterDuff.Mode.SRC_ATOP); // for filled stars
+//        stars.getDrawable(1).setColorFilter(Color.parseColor("#e7c31b"), PorterDuff.Mode.SRC_ATOP); // for half filled stars
+//        stars.getDrawable(0).setColorFilter(Color.parseColor("#c2c2c2"), PorterDuff.Mode.SRC_ATOP); // for empty stars
 
         back_btn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -64,8 +129,6 @@ public class PostActivity extends AppCompatActivity {
                 SimpleDateFormat sformat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
                 Date now = new Date();
                 String getTime = sformat.format(now);
-
-                int rating = (int) feel_rate.getRating();
 
                 try{
                     String jsonString = new JSONObject()
