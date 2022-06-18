fb0ab8f4e13a49ad879d070cbf32105be38308ce
2haein <eehaein2@gmail.com>
2022-06-19 02:45:50 +0900

    organize folders

--- backend/Readme.md
+++
@@ -1,2 +0,0 @@
-# comfort_feeling
-## 백엔드 스프링부트 서버
--- backend/src/main/java/com/codeboogie/comfortbackend/api/member/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.codeboogie.comfortbackend.api.member.service;
-
-import com.codeboogie.comfortbackend.common.member.domain.dto.MemberDTO;
-
-public interface MemberService {
-    boolean createMember(MemberDTO memberDTO);
-}
